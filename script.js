080d62fe04ab0fd50bc1fc224545f1ebbfcdc610
Giovanni Guimarães <98474365+giovannniguimaraes@users.noreply.github.com>
2022-07-19 14:15:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var numero = 100
+var numero = 100 //digite aqui o número desejado e aperte F8 para executar o código no terminal.
 var binario = (numero).toString(2)
 var res = binario.split("",6)
+
 if(numero > 31 ){
     if(res[0]==res[5] && res[1]==res[4] && res[2]==res[3]){
             console.log(`O número ${numero} é palíndromo!`)
@@ -8,14 +9,14 @@ if(numero > 31 ){
             console.log(`O número ${numero} não é palíndromo!`)
     }
 }
-if(numero >15 && numero <32){
+if(numero > 15 && numero < 32){
     if(res[0]==res[4] && res[1]==res[3]){
         console.log(`O número ${numero} é palíndromo!`)
     }else{
         console.log(`O número ${numero} não é palíndromo!`)
     }
 }
-if(numero >7 && numero < 16){
+if(numero > 7 && numero < 16){
     if(res[0]==res[3] && res[1]==res[2]){
         console.log(`O número ${numero} é palíndromo!`)
     }else{
@@ -23,7 +24,7 @@ if(numero >7 && numero < 16){
     }
 }
 
-if(numero >3 && numero < 8){      
+if(numero > 3 && numero < 8){      
     if (res[0]==res[2]){
         console.log(`O número ${numero} é palíndromo!`)
     }else{
